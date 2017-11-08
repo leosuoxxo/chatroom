1967f514816ef04c3ab06e35c6a6413896b687ed
Leo Su <leo.suoxxo@gmail.com>
2017-11-09 00:40:26 +0800

    add controller

--- server/controllers/users/UserController.js
+++ server/controllers/users/UserController.js
@@ -1,21 +1,17 @@
-const mongoose = require('mongoose');
 // import md5 from 'md5';
-require('models/user.js');
-
-const UserModel = mongoose.model('User');
-// const UserModel =
-// require('models/user.js');
+const UserModel = require('../../models/user.js');
 
 class UserController {
   // 用户登入
-  // static async login(ctx) {
-  // await UserModel.find({ Nickname: 'Leo' }, (err) => {
-  //   if (err) {
-  //     throw err;
-  //   }
-  //   console.log(ctx.body);
-  // });
-  // }
+  static async login(ctx) {
+    // await UserModel.find({ Nickname: 'Leo' }, (err) => {
+    //   if (err) {
+    //     throw err;
+    //   }
+    //   console.log(ctx.body);
+    // });
+    console.log(123);
+  }
 }
 
 module.exports = UserController;
