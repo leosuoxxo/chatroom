0e950409d3940716f10916c185756c5974167764
DESKTOP-S6LGJ91\嗨丸 <http://kyle@git.jfetek.com/Uauor.git>
2017-11-09 22:30:45 +0800

    增加註冊 登入鈕  但是顏色切換還有問題

--- src/components/SignAndLog/SignAndLog.js
+++ src/components/SignAndLog/SignAndLog.js
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const BtnBox = styled.ul`
 display:flex;
+width:50vw;
 align-items:center;
 justify-content:flex-start;
 font-weight:900;
 font-size:5vw;
 letter-spacing:-0.5vw;
 `;
+const MyBtn = styled.li`
+width:30%;
+font-size:5vw;
+display:flex;
+align-items:center;
+justify-content:flex-start;
+cursor:pointer;
+`;
+
+class SignAndLog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { Scolor: 'black', Lcolor: 'grey' };
+    this.chooseFun = this.chooseFun.bind(this);
+    localStorage.setItem('fuck', this.state.Scolor);
+  }
 
 
-const SignAndLog = () => (
-  <BtnBox>Just Talk</BtnBox>
-);
+  chooseFun(e) {
+    const myName = e.target.dataset.name;
+    if (myName === 'sign') {
+      this.setState({
+        Scolor: 'black',
+        Lcolor: 'grey'
+      });
+      localStorage.setItem('status', myName);
+    } else {
+      this.setState({
+        Scolor: 'grey',
+        Lcolor: 'black'
+      });
+      localStorage.setItem('status', myName);
+    }
+  }
+
+  render() {
+    return (
+      <BtnBox>
+        <MyBtn onClick={this.chooseFun} data-name="sign" style={{ color: this.state.Scolor }}>註冊</MyBtn>
+        <MyBtn onClick={this.chooseFun} data-name="log" style={{ color: this.state.Lcolor }}>登入</MyBtn>
+      </BtnBox>
+    );
+  }
+}
+
 
 export default SignAndLog;
--- src/containers/SignAndLog/Main.js
+++ src/containers/SignAndLog/Main.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Title from '../../components/Title/Title';
+import SignAndLog from '../../components/SignAndLog/SignAndLog';
 
 const MainPage = () => (
-  <div><Title /></div>
+  <div><Title /><SignAndLog /></div>
 );
 
 export default MainPage;
