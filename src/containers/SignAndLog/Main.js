018c028e2066b207c2259e113878d1692285093f
DESKTOP-S6LGJ91\嗨丸 <http://kyle@git.jfetek.com/Uauor.git>
2017-11-11 20:44:25 +0800

    註冊登入改變顏色完成

--- src/containers/SignAndLog/Main.js
+++ src/containers/SignAndLog/Main.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
 import Title from '../../components/Title/Title';
 import SignAndLog from '../../components/SignAndLog/SignAndLog';
 
-const MainPage = () => (
-  <div><Title /><SignAndLog /></div>
-);
+class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { Scolor: 'black', Lcolor: 'grey' };
+    this.chooseFun = this.chooseFun.bind(this);
+    localStorage.setItem('fuck', this.state.Scolor);
+  }
 
-export default MainPage;
+
+  chooseFun(e) {
+    const myName = e.target.dataset.name;
+    if (myName === 'sign') {
+      this.setState({
+        Scolor: 'black',
+        Lcolor: 'grey'
+      });
+      localStorage.setItem('status', myName);
+    } else {
+      this.setState({
+        Scolor: 'grey',
+        Lcolor: 'black'
+      });
+      localStorage.setItem('status', myName);
+    }
+  }
+
+  render() {
+    return (
+      <div><Title /><SignAndLog chooseFun={this.chooseFun} Scolor={this.state.Scolor} Lcolor={this.state.Lcolor} /></div>
+    );
+  }
+}
+
+ReactDom.render(<MainPage />, document.getElementById('app'));
