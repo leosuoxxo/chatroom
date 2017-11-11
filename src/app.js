4298bdd550c0d1bf16846292e37ea6f9296be3c4
DESKTOP-S6LGJ91\嗨丸 <http://kyle@git.jfetek.com/Uauor.git>
2017-11-11 21:31:14 +0800

    bug版本

--- src/app.js
+++ src/app.js
@@ -5,34 +5,12 @@ import './assets/sass/main.sass';
 import SignAndLog from './components/SignAndLog/SignAndLog';
 
 class MainPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { Scolor: 'black', Lcolor: 'grey' };
-    this.chooseFun = this.chooseFun.bind(this);
-    localStorage.setItem('fuck', this.state.Scolor);
-  }
-
-
-  chooseFun(e) {
-    const myName = e.target.dataset.name;
-    if (myName === 'sign') {
-      this.setState({
-        Scolor: 'black',
-        Lcolor: 'grey'
-      });
-      localStorage.setItem('status', myName);
-    } else {
-      this.setState({
-        Scolor: 'grey',
-        Lcolor: 'black'
-      });
-      localStorage.setItem('status', myName);
-    }
-  }
-
   render() {
     return (
-      <div><Title /><SignAndLog chooseFun={this.chooseFun} Scolor={this.state.Scolor} Lcolor={this.state.Lcolor} /></div>
+      <div>
+        <Title />
+        <SignAndLog />
+      </div>
     );
   }
 }
--- src/components/SignAndLog/SignAndLog.js
+++ src/components/SignAndLog/SignAndLog.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 
 const BtnBox = styled.ul`
 display:flex;
@@ -22,21 +21,36 @@ transition:0.3s ease;
 `;
 
 class SignAndLog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { Scolor: 'black', Lcolor: 'grey' };
+    this.chooseFun = this.chooseFun.bind(this);
+  }
+
+
+  chooseFun(e) {
+    const myName = e.target.dataset.name;
+    if (myName === 'sign') {
+      this.setState({
+        Scolor: 'black',
+        Lcolor: 'grey'
+      });
+    } else {
+      this.setState({
+        Scolor: 'grey',
+        Lcolor: 'black'
+      });
+    }
+  }
   render() {
     return (
       <BtnBox>
-        <MyBtn onClick={this.props.chooseFun} style={{ color: this.props.Scolor }} data-name="sign">註冊</MyBtn>
-        <MyBtn onClick={this.props.chooseFun} style={{ color: this.props.Lcolor }} data-name="log">登入</MyBtn>
+        <MyBtn onClick={this.chooseFun} style={{ color: this.state.Scolor }} data-name="sign">註冊</MyBtn>
+        <MyBtn onClick={this.chooseFun} style={{ color: this.state.Lcolor }} data-name="log">登入</MyBtn>
       </BtnBox>
     );
   }
 }
-
-SignAndLog.propTypes = {
-  chooseFun: PropTypes.func.isRequired,
-  Scolor: PropTypes.string.isRequired,
-  Lcolor: PropTypes.string.isRequired
-};
 
 
 export default SignAndLog;
