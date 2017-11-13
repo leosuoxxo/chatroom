b030640744ed8b766de2d4d8dac2b71a3e6e1f95
DESKTOP-S6LGJ91\嗨丸 <http://kyle@git.jfetek.com/Uauor.git>
2017-11-13 20:25:46 +0800

    檢查格式完成   安裝axios完成

--- src/app.js
+++ src/app.js
@@ -17,15 +17,30 @@ class MainPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      Scolor: 'grey', Lcolor: 'black', display: 'none', placeholder: '你的名稱', password: '你的密碼'
+      Scolor: 'grey',
+      Lcolor: 'black',
+      display: 'none',
+      placeholder: '你的名稱',
+      password: '你的密碼',
+      hint: 'none',
+      inputBorder: 'none',
+      pass: 'none',
+      passBorder: 'none',
+      email: 'none',
+      emailBorder: 'none',
+      account: 'none',
+      accountBorder: 'none'
     };
     this.chooseFun = this.chooseFun.bind(this);
+    this.checkFunc = this.checkFunc.bind(this);
   }
 
 
   chooseFun(e) {
     const myName = e.target.dataset.name;
     if (myName === 'sign') {
+      // 顯示註冊欄位
+
       this.setState({
         Scolor: 'black',
         Lcolor: 'grey',
@@ -34,13 +49,60 @@ class MainPage extends Component {
         password: '請輸入6~25字元的密碼'
       });
     } else {
+      // 顯示登入欄位
+
       this.setState({
         Scolor: 'grey',
         Lcolor: 'black',
         display: 'none',
         placeholder: '你的名稱',
-        password: '你的密碼'
+        password: '你的密碼',
+        pass: 'none',
+        passBorder: 'none',
+        account: 'none',
+        accountBorder: 'none'
       });
+    }
+  }
+  checkFunc() {
+    const myIndex = document.getElementById('myAccount').placeholder;
+    const myPassword = document.getElementById('myPassword').value;
+    const secondPassword = document.getElementById('secondPassword').value;
+    const emailReg = /^[^\s]+@[^\s]+\.[^\s]{2,3}$/;
+    const myEmail = document.getElementById('myEmail').value;
+    const myAccount = document.getElementById('myAccount').value;
+    if (myIndex === '你的名稱') {
+      // 登入
+    } else {
+      // 註冊
+
+      // 檢查帳號格式
+      if (myAccount) {
+        this.setState({ account: 'none', accountBorder: 'none' });
+      } else {
+        this.setState({ account: 'flex', accountBorder: '1px solid #ff0000' });
+      }
+
+      // 檢查信箱格式
+      if (emailReg.test(myEmail)) {
+        this.setState({ email: 'none', emailBorder: 'none' });
+      } else {
+        this.setState({ email: 'flex', emailBorder: '1px solid #ff0000' });
+      }
+
+      // 檢查密碼格式
+      if (myPassword.length >= 6 && myPassword.length <= 25) {
+        this.setState({ pass: 'none', passBorder: 'none' });
+      } else {
+        this.setState({ pass: 'flex', passBorder: '1px solid #ff0000' });
+      }
+
+      // 檢查重複密碼格式
+      if (myPassword === secondPassword) {
+        this.setState({ hint: 'none', inputBorder: 'none' });
+      } else {
+        this.setState({ hint: 'flex', inputBorder: '1px solid #ff0000' });
+      }
     }
   }
 
@@ -49,8 +111,8 @@ class MainPage extends Component {
       <div>
         <Title />
         <SignAndLog chooseFun={this.chooseFun} Scolor={this.state.Scolor} Lcolor={this.state.Lcolor} />
-        <FillField display={this.state.display} placeholder={this.state.placeholder} password={this.state.password} />
-        <Confirmbutton />
+        <FillField display={this.state.display} placeholder={this.state.placeholder} password={this.state.password} hint={this.state.hint} inputBorder={this.state.inputBorder} pass={this.state.pass} passBorder={this.state.passBorder} email={this.state.email} emailBorder={this.state.emailBorder} account={this.state.email} accountBorder={this.state.emailBorder} />
+        <Confirmbutton checkFunc={this.checkFunc} />
       </div>
     );
   }
